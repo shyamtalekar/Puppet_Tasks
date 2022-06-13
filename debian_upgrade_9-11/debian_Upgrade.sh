007f9d25e05d6184cf6a746febe85b2dfaae7793
ubuntu <41911607+shyamtalekar@users.noreply.github.com>
2022-06-13 15:42:03 +0530

    Create debian_Upgrade.sh

---
+++ debian_upgrade_9-11/debian_Upgrade.sh
@@ -0,0 +1,26 @@
+# This script sets up a Upgrade to Debian 11 Bullseye
+
+set -e
+
+echo "update all the packages on your Debian system"
+sudo apt update -y
+sudo apt full-upgrade -y
+
+echo "remove any unused dependencies"
+sudo apt autoremove -y
+
+echo "take backup and replace all instances of stretch with bullseye"
+sudo cp -v /etc/apt/sources.list /root/
+sudo cp -rv /etc/apt/sources.list.d/ /root/
+sudo sed -i 's/stretch/bullseye/g' /etc/apt/sources.list
+sudo sed -i 's/stretch/bullseye/g' /etc/apt/sources.list.d/*
+
+echo "upgrade to Debian 11 Bullseye"                                                                                                                         sudo apt update -y                                                                                                                                          sudo apt upgrade -y
+
+echo "apt full-upgrade to complete the upgrade to Debian 11"
+sudo apt full-upgrade
+
+echo "reboot your system"
+sudo reboot
+
+echo "Finished upgrade to Debian 11(Bullseye)"  
